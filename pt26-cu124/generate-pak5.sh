a87240bcb215e8874ebaf5469aaf1820ce8717de
YAN Wenkun <code@yanwk.fun>
2025-01-21 16:40:38 +0800

    Update deps for updated SF3D

--- pt26-cu124/generate-pak5.sh
+++ pt26-cu124/generate-pak5.sh
@@ -6,6 +6,7 @@ echo '#' > pak5.txt
 array=(
 https://github.com/comfyanonymous/ComfyUI/raw/refs/heads/master/requirements.txt
 https://github.com/MrForExample/ComfyUI-3D-Pack/raw/a35a737676cf3cbb23360d98032870e242dae199/requirements.txt
+https://github.com/Stability-AI/stable-fast-3d/raw/refs/heads/main/requirements.txt
 https://github.com/edenartlab/eden_comfy_pipelines/raw/refs/heads/main/requirements.txt
 https://github.com/kijai/ComfyUI-KJNodes/raw/refs/heads/main/requirements.txt
 https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/raw/refs/heads/main/requirements.txt
@@ -28,6 +29,6 @@ sed -i 's/_/-/g' pak5.txt
 sort -ufo pak5.txt pak5.txt
 
 # Remove duplicate items, compare to pak4.txt.txt
-grep -Fixv -f pak4.txt.txt pak5.txt > temp.txt && mv temp.txt pak5.txt
+grep -Fixv -f pak4.txt pak5.txt > temp.txt && mv temp.txt pak5.txt
 
 echo "<pak5.txt> generated. Check before use."
